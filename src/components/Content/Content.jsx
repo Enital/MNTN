10038a3392712d14d791679396e4708f08083250
Enital <117684114+Enital@users.noreply.github.com>
2023-10-19 19:16:51 +0300

    add 01 block

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,7 +1,33 @@
 import css from "./content.module.css";
 
 function Content() {
-  return <div className={css.content}>content</div>;
+  return (
+    <div className={css.content}>
+      <div className={css.blockOne}>
+        <ul>
+          <li>
+            <h3 className={css.opacityTitle}>01</h3>
+            <p className={css.yellowTitle}>GET STARTED</p>
+          </li>
+          <li>
+            <h4 className={css.blockTitle}>What level of hiker are you?</h4>
+            <p className={css.blockText}>
+              Determining what level of hiker you are can be an important tool
+              when planning future hikes. This hiking level guide will help you
+              plan hikes according to different hike ratings set by various
+              websites like All Trails and Modern Hiker. What type of hiker are
+              you – novice, moderate, advanced moderate, expert, or expert
+              backpacker?
+            </p>
+            <img src="../../images/01.png" alt="01" width={566} height={720} />
+          </li>
+        </ul>
+      </div>
+
+      <h3>02</h3>
+      <h3>03</h3>
+    </div>
+  );
 }
 
 export default Content;
