6cb2fb0ef90dcb7a1393419b94df1d2aa2c27f6a
Enital <117684114+Enital@users.noreply.github.com>
2023-10-20 18:07:31 +0300

    add content

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,78 +1,105 @@
 import lineBlock from "../../images/line_block.png";
 import img01 from "../../images/01.png";
+import { BsArrowRight } from "react-icons/bs";
 
 import css from "./content.module.css";
 
 function Content() {
   return (
-    <div className={css.content}>
-      <div className={css.blockOne}>
-        <ul className={css.list}>
-          <li className={css.upperTitle}>
+    <>
+      <div className={css.contentBG}></div>
+      <div className={css.content}>
+        <div className={css.contentBlockOne}>
+          <div className={css.list}>
             <h3 className={css.opacityTitle}>01</h3>
-            <img src={lineBlock} alt="line" className={css.lineBlock} />
-            <p className={css.yellowTitle}>GET STARTED</p>
-          </li>
-          <li className={css.bottomBlock}>
-            <h4 className={css.blockTitle}>What level of hiker are you?</h4>
-            <p className={css.blockText}>
-              Determining what level of hiker you are can be an important tool
-              when planning future hikes. This hiking level guide will help you
-              plan hikes according to different hike ratings set by various
-              websites like All Trails and Modern Hiker. What type of hiker are
-              you – novice, moderate, advanced moderate, expert, or expert
-              backpacker?
-            </p>
-            <div className={css.imgBlock}></div>
-          </li>
-          <img src={img01} alt="01" className={css.img} />
-        </ul>
-      </div>
+            <ul>
+              <li className={css.upperTitle}>
+                <img src={lineBlock} alt="line" className={css.lineBlock} />
+                <p className={css.yellowTitle}>GET STARTED</p>
+              </li>
+              <li className={css.bottomBlock}>
+                <h4 className={css.blockTitle}>What level of hiker are you?</h4>
+                <p className={css.blockText}>
+                  Determining what level of hiker you are can be an important
+                  tool when planning future hikes. This hiking level guide will
+                  help you plan hikes according to different hike ratings set by
+                  various websites like All Trails and Modern Hiker. What type
+                  of hiker are you – novice, moderate, advanced moderate,
+                  expert, or expert backpacker?
+                </p>
+                <button className={css.contentButton}>
+                  <span className={css.yellowTitle}>read more</span>
+                  <BsArrowRight color="#FBD784" />
+                </button>
+                {/* <div className={css.imgBlock}></div> */}
+              </li>
+            </ul>
 
-      <div className={css.blockTwo}>
-        <ul className={css.list}>
-          <li className={css.upperTitle}>
+            <img src={img01} alt="01" className={css.img} />
+          </div>
+        </div>
+
+        <div className={css.contentBlock}>
+          <div className={css.list}>
             <h3 className={css.opacityTitle}>02</h3>
-            <p className={css.yellowTitle}>GET STARTED</p>
-          </li>
-          <li className={css.bottomBlock}>
-            <h4 className={css.blockTitle}>What level of hiker are you?</h4>
-            <p className={css.blockText}>
-              Determining what level of hiker you are can be an important tool
-              when planning future hikes. This hiking level guide will help you
-              plan hikes according to different hike ratings set by various
-              websites like All Trails and Modern Hiker. What type of hiker are
-              you – novice, moderate, advanced moderate, expert, or expert
-              backpacker?
-            </p>
-            <div className={css.imgBlock}></div>
-          </li>
-          <img src={img01} alt="01" className={css.img} />
-        </ul>
-      </div>
+            <ul>
+              <li className={css.upperTitle}>
+                <img src={lineBlock} alt="line" className={css.lineBlock} />
+                <p className={css.yellowTitle}>GET STARTED</p>
+              </li>
+              <li className={css.bottomBlock}>
+                <h4 className={css.blockTitle}>What level of hiker are you?</h4>
+                <p className={css.blockText}>
+                  Determining what level of hiker you are can be an important
+                  tool when planning future hikes. This hiking level guide will
+                  help you plan hikes according to different hike ratings set by
+                  various websites like All Trails and Modern Hiker. What type
+                  of hiker are you – novice, moderate, advanced moderate,
+                  expert, or expert backpacker?
+                </p>
+                <button className={css.contentButton}>
+                  <span className={css.yellowTitle}>read more</span>
+                  <BsArrowRight color="#FBD784" />
+                </button>
+                {/* <div className={css.imgBlock}></div> */}
+              </li>
+            </ul>
 
-      <div className={css.blockThree}>
-        <ul className={css.list}>
-          <li className={css.upperTitle}>
+            <img src={img01} alt="01" className={css.img} />
+          </div>
+        </div>
+
+        <div className={css.contentBlock}>
+          <div className={css.list}>
             <h3 className={css.opacityTitle}>03</h3>
-            <p className={css.yellowTitle}>GET STARTED</p>
-          </li>
-          <li className={css.bottomBlock}>
-            <h4 className={css.blockTitle}>What level of hiker are you?</h4>
-            <p className={css.blockText}>
-              Determining what level of hiker you are can be an important tool
-              when planning future hikes. This hiking level guide will help you
-              plan hikes according to different hike ratings set by various
-              websites like All Trails and Modern Hiker. What type of hiker are
-              you – novice, moderate, advanced moderate, expert, or expert
-              backpacker?
-            </p>
-            <div className={css.imgBlock}></div>
-          </li>
-          <img src={img01} alt="01" className={css.img} />
-        </ul>
+            <ul>
+              <li className={css.upperTitle}>
+                <img src={lineBlock} alt="line" className={css.lineBlock} />
+                <p className={css.yellowTitle}>GET STARTED</p>
+              </li>
+              <li className={css.bottomBlock}>
+                <h4 className={css.blockTitle}>What level of hiker are you?</h4>
+                <p className={css.blockText}>
+                  Determining what level of hiker you are can be an important
+                  tool when planning future hikes. This hiking level guide will
+                  help you plan hikes according to different hike ratings set by
+                  various websites like All Trails and Modern Hiker. What type
+                  of hiker are you – novice, moderate, advanced moderate,
+                  expert, or expert backpacker?
+                </p>
+                <button className={css.contentButton}>
+                  <span className={css.yellowTitle}>read more</span>
+                  <BsArrowRight color="#FBD784" />
+                </button>
+                {/* <div className={css.imgBlock}></div> */}
+              </li>
+            </ul>
+
+            <img src={img01} alt="01" className={css.img} />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
