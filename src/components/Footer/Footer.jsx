9921fc936aa51bbaa374b82761667d687a53719b
Enital <117684114+Enital@users.noreply.github.com>
2023-10-22 15:13:34 +0300

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,38 @@
+import css from "./footer.module.css";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={css.footer}>
+      <div className={css.footerContent}>
+        <div className={css.footerLogo}>
+          <h4 className={css.logo}>MNTN</h4>
+          <p className={css.logoText}>
+            Get out there & discover your next slope, mountain & destination!
+          </p>
+          <p className={css.copyright}>
+            Copyright 2019 MNTN, Inc. Terms & Privacy
+          </p>
+        </div>
+        <div className={css.footerLinks}>
+          <h4 className={css.yellowTitle}>More on The Blog</h4>
+          <ul>
+            <li className={css.list}>About MNTN</li>
+            <li className={css.list}>Contributors & Writers</li>
+            <li className={css.list}>Write For Us</li>
+            <li className={css.list}>Contact Us</li>
+            <li className={css.list}>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className={css.footerLinks}>
+          <h4 className={css.yellowTitle}>More on MNTN</h4>
+          <ul>
+            <li className={css.list}>The Team</li>
+            <li className={css.list}>Jobs</li>
+            <li className={css.list}>Press</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
